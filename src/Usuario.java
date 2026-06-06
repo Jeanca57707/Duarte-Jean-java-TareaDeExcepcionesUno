52df359e2c1f207eb1e5b81fefbae0e0d8795afa
Jeanca57707 <jean_duarte@ucne.edu.do>
2026-06-05 21:48:00 -0400

    Clase Usuario

---
+++ src/Usuario.java
@@ -0,0 +1,66 @@
+public class Usuario{
+
+    //Atributos del Usuario
+
+    private String nombre;
+    private int edad;
+    private String email;
+    private double salario;
+
+
+    //Cosntructor vacio
+    public Usuario(){
+
+        setNombre("");
+        setEdad(0);
+        setEmail("");
+        setSalario(0.0);
+    }
+
+    //Constructor con parámetros
+    public Usuario(String nombre, int edad, String email, double salario){
+
+        setNombre(nombre);
+        setEdad(edad);
+        setEmail(email);
+        setSalario(salario);
+    }
+
+
+    //Setters & Getters
+
+    public void setNombre(String nombre){
+
+        this.nombre = nombre;
+    }
+    public void setEdad(int edad){
+
+        this.edad = edad;
+    }
+    public void setEmail(String email){
+
+        this.email = email;
+    }
+    public void setSalario(double salario){
+
+        this.salario = salario;
+    }
+
+    public String getNombre(){
+
+        return nombre.toUpperCase();
+    }
+    public int getEdad(){
+
+        return edad;
+    }
+    public String getEmail(){
+
+        return email;
+    }
+    public double getSalario(){
+
+        return salario;
+    }
+    
+}
